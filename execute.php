1bb4513be7802892e2849336461b6cf60b0f1758
szanella68 <45138935+szanella68@users.noreply.github.com>
2018-11-20 14:49:50 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -109,6 +109,6 @@ else
 
 
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters = array('chat_id' => $chatId, "text" => $response);
 $parameters["method"] = "sendMessage";
 echo json_encode($parameters);
