f424199da4afbf03b88fc53dddf43615ef0a11fc
szanella68 <45138935+szanella68@users.noreply.github.com>
2018-11-21 14:22:24 +0100

    Update registerindex.php

--- registerindex.php
+++ registerindex.php
@@ -1,6 +1,6 @@
 <?php
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://zanellazenbot.herokuapp.com/index.php';
+$WEBHOOK_URL = 'https://zanellazenbot.herokuapp.com/index1.php';
 $BOT_TOKEN = '681369794:AAGuUAUjHDGAOsmo3GXX7VSdO9E5-OTISYE';
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
