e14d30f941bd2c32f15036e0dfb78056dfb7886c
szanella68 <45138935+szanella68@users.noreply.github.com>
2018-11-21 14:47:46 +0100

    Update Index1.php

--- Index1.php
+++ Index1.php
@@ -34,7 +34,7 @@
    }
    $esempiotastierainline = '[{"text":"Testo","url":"http://yt.alexgaming.me"},{"text":"Inline","switch_inline_query":"Ciao!"}],[{"text":"Testo","callback_data":"StampaMessaggio"},{"text":"Modifica Messaggio","callback_data":"ModificaMessaggio"}]';
   switch($text){
-    case "/start":
+    case "/start": 
         sendMessage($chatId,"Weyla!");
         break;
     case "/tastiera":
